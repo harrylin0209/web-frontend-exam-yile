5f16837879ae2b884e1b6edf732779267ebf8450
harrylin0209 <87509419+harrylin0209@users.noreply.github.com>
2026-05-22 18:05:12 +0800

    feat: add hero illustration

--- src/components/HeroIllustration.jsx
+++ src/components/HeroIllustration.jsx
@@ -1,8 +1,77 @@
+import backgroundImage from '../assets/background.png';
+import characterImage from '../assets/character.png';
+import characterWhiteImage from '../assets/character-white.png';
+import logoImage from '../assets/logo.png';
+
 function HeroIllustration() {
   return (
-    <div>
-      Hero Illustration
-    </div>
+    <section className="w-full overflow-hidden">
+      {/* responsive wrapper */}
+      <div className="relative w-full">
+        {/* Figma canvas */}
+        <div
+          className="
+            relative
+            aspect-[1440/823]
+            w-full
+            max-w-[1440px]
+            origin-top-left
+          "
+        >
+          {/* background */}
+          <img
+            src={backgroundImage}
+            alt="background"
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+
+          {/* white base */}
+          <img
+            src={characterWhiteImage}
+            alt="character white"
+            className="
+              absolute
+              left-0
+              top-0
+              h-full
+              w-auto
+              z-10
+              object-contain
+            "
+          />
+
+          {/* character */}
+          <img
+            src={characterImage}
+            alt="character"
+            className="
+              absolute
+              left-0
+              top-0
+              h-full
+              w-auto
+              z-20
+              object-contain
+              mix-blend-multiply
+            "
+          />
+
+          {/* logo */}
+          <img
+            src={logoImage}
+            alt="logo"
+            className="
+              absolute
+              left-[56.74%]
+              top-[42.53%]
+              w-[37.5%]
+              h-auto
+              z-30
+            "
+          />
+        </div>
+      </div>
+    </section>
   );
 }
 
